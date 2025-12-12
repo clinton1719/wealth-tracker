79e8e7e5d4575b9a01c4e9cbb004488a7e8cab97
clinton1719 <clintoncleetus17@gmail.com>
2025-12-12 10:09:06 +0530

    test

--- backend/src/main/java/com/backend/wealth_tracker/WealthTrackerApplication.java
+++ backend/src/main/java/com/backend/wealth_tracker/WealthTrackerApplication.java
@@ -2,12 +2,10 @@ package com.backend.wealth_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@PropertySource(value = "file:${user.dir}/.env")
 public final class WealthTrackerApplication {
   private WealthTrackerApplication() {}
 
